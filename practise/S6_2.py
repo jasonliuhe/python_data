dcf81793e947dd699c8e8ff030f55a9cec44146b
He Liu <walkerliuhe@gmail.com>
2021-01-21 15:34:47 -0500

    Numpy 1

---
+++ practise/S6_2.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+
+from numpy.random import randn
+
+np.random.seed(101)
+df = pd.DataFrame(randn(5, 4), ['A', 'B', 'C', 'D', 'E'], ['W', 'X', 'Y', 'Z'])
+# print(df['W'])
+# print(type(df['W']))
+# print(type(df))
+# print(df[['W', 'Z']])
+df['new'] = df['W'] + df['Y']
+# print(df['new'])
+df.drop('new', axis=1, inplace=True)
+# print(df)
+# print(df.drop('E', axis=0))
+# print(df.shape)
+# print(df[['Z', 'X']])
+# print(df.loc['A'])
+# print(df.iloc[2])
+# print(df.loc['B', 'Y'])
+# print(df.loc[['A', 'B'], ['W', 'Y']])
+# print(df > 0)
+booldf = df > 0
+# print(booldf)
+# print(df[booldf])
+# print(df[df > 0])
+# print(df['W'] > 0)
+# print(df[df['W'] > 0])
+# print(df[df['Z'] < 0])
+resultdf = df[df['W'] > 0]
+# print(resultdf['X'])
+print(df[df['W'] > 0][['Y', 'X']])
+boolser = df['W'] > 0
+result = df[boolser]
+print(result)
